937b4ea0ae6b80099f97fe62db5259e91e172c11
Sutanay Nandi <sutanaynandi1@gmail.com>
2020-07-27 17:36:34 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+  alert("Reps till Failure and 2 sets each!");
+  $("#accordion").accordion({
+    heightStyle: "content",
+           collapsible: true ,
+           autoHeight: false,
+           clearStyle: true,
+  });
+  $(".chhotu").accordion({
+    heightStyle: "content",
+           collapsible: true ,
+           autoHeight: false,
+           clearStyle: true,
+  });
+
+});
